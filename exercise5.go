388cefaa1363dd27f429727a4125ac88f0e2f1aa
Sam Rogerson <samr@cern.ch>
2013-04-22 18:39:51 +0100

    ex5

---
+++ exercise5.go
@@ -0,0 +1,28 @@
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+package main
+
+import "fmt"
+
+func gcd(a, b int64) (t int64) {
+    for b != 0 {
+        t = b
+        b = a % b
+        a = t
+    }
+    return
+}
+
+func lcm(a, b int64) int64 {
+    return (a * b / gcd(a, b))
+}
+
+func main() {
+    l := int64(1)
+    for i:=2; i < 21; i++ {
+        l = lcm(l,int64(i)) 
+        fmt.Println(l, i)
+    }
+    fmt.Println(l)
+}
