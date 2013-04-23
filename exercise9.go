530e3b89306087141a1af764009ab1f3c48711ac
Sam Rogerson <samr@cern.ch>
2013-04-23 11:06:45 +0100

    solve 9

--- exercise9.go
+++ exercise9.go
@@ -1,12 +1,20 @@
 /*
 A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
 
-a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
+
+/*
+
+a^2 + b^2 - c^2 = 0
+a + b +  = 1000c  = 1000
+
+*/
+
 
 package main
 
@@ -14,5 +22,23 @@ import (
     "fmt"
 )
 
+func pythag(total int) (a,b,c int) {
+    for a= 1; a < total - 5; a++ {
+        for b=a+1; b + a < total - 3; b++ {
+            for c= b+1; c < total - 3;  c++ {
+                if a+b+c == 1000 && (a*a + b*b == c*c) {
+                    return a,b,c
+                }
+            }
+        }
+    }
+    return 0,0,0
+}
+
+
+
 func main() {
+    a,b,c := pythag(1000)
+    fmt.Println(a,b,c)
+    fmt.Println(a*b*c)
 }
