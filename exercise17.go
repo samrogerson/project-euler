89110792ab2600f81457ed44bc58d4de36cb8aab
Sam Rogerson <samr@cern.ch>
2013-04-23 18:51:18 +0100

    solve ex17

--- exercise17.go
+++ exercise17.go
@@ -14,24 +14,33 @@ import (
     //"strings"
 )
 
+var hundred = 7
+
 var words = map[string] int {
     "1": 3, "2": 3, "3": 5, "4": 4, "5": 4, "6": 3, "7": 5, "8": 5, "9": 4,
-    "10": 3, "11": 6, "12": 6, "13": 8, "14": 8, "15": 7, "16": 7, "18": 8,
-    "19": 4, "20": 6, "30": 6, "40": 5, "50": 5, "60": 5, "70": 7, "80": 6, "90": 6,
-    "hundred": 7,
-    "thousand": 8,
+    "10": 3, "11": 6, "12": 6, "13": 8, "14": 8, "15": 7, "16": 7, "17": 9, "18": 8,
+    "19": 8, "20": 6, "30": 6, "40": 5, "50": 5, "60": 5, "70": 7, "80": 6, "90": 6,
 }
 
-func countnumber(num int) (letters int) {
+var orders = map[int] int {
+    1 : 0,
+    2 : 8,  // len("thousand")
+    3 : 7,  // len("million")
+    4 : 7,  // len("billion")
+}
+
+func SeparateByThrees(num int) [][]rune {
     runes := []rune(strconv.Itoa(num)) 
 
     nrunes := len(runes)
     ngroups := nrunes / 3
 
-    groups := [][]rune{[]rune{0,0,0}}
+    groups := make([][]rune,ngroups)
+
+    offset :=0
     for i := 0; i<ngroups; i++ {
-        fmt.Println(i, nrunes)
-        groups[i] = []rune{runes[nrunes-3*i-3],runes[nrunes-3*i-2],runes[nrunes-3*i-1]}
+        offset = 3*(i+1)
+        groups[i] = []rune{runes[nrunes-offset],runes[nrunes-offset+1],runes[nrunes-offset+2]}
     }
 
     if ngroups * 3 != nrunes {
@@ -47,58 +56,55 @@ func countnumber(num int) (letters int) {
         { ttt } hundreds of thousands 
         { mmm } hundreds of millions
     */
-    for i, g := range groups {
-        fmt.Println(i, string(g))
-    }
-    return
+
+    return groups
 }
+
+func NumberAsLetterCount(groups [][]rune) int {
+    total := 0
+
+    for i := 1; i <= len(groups); i++ { // adding millions / billions
+        total += orders[i]
+    }
+
+    s := ""
+    for _, g := range groups {
+        s = string(g[0])
+        if len(g) == 3 {
+            if s != "0" {
+                total += words[s] + hundred  // X hundred
+            }
+            if string(g[1]) != "0" || string(g[2]) != "0" {
+                total += 3 //and
+            }
+        }
+        if len(g) >=2 {
+            // get the last 2 digits
+            numpair := []rune{ g[len(g)-2], g[len(g)-1] } // e.g. {X27} -> {27}
+            if string(numpair[0]) ==  "0" { //less than ten
+                total += words[string(numpair[1])]
+            } else if string(numpair[0]) == "1" { // teen
+                total += words[string(numpair)]
+            } else { //2X, 3X, 4X...
+                total += words[string(numpair[0])+"0"] + words[string(numpair[1])]
+            }
+        } else {
+            total += words[s]
+        }
+        
+    }
+    return total
+}
+
 
 func main() {
-    countnumber(55544433)
+    totalletters :=0
+    temp := 0
+    for num := 1; num<=1000; num++ {
+        groups := SeparateByThrees(num)
+        temp = NumberAsLetterCount(groups)
+        totalletters += temp
+        fmt.Println(num, temp)
+    }
+    fmt.Println(totalletters)
 }
-
-
-
-    //sum := 0
-    //val := 0
-    //for i:=1; i<=1000; i++ {
-        //val = countnumber(i)
-        //fmt.Println(i, val)
-        //sum += val
-    //}
-    //fmt.Println(sum)
-
-
-    //runes := []rune(strconv.Itoa(num))
-    //// reverse so we can work from the bottom up
-	//for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		//runes[i], runes[j] = runes[j], runes[i]
-	//} 
-    //// do first 2 then last 1 or 2
-    //val := ""
-    //modifier := ""
-    //letters = 0
-    //for _, r  := range runes {
-        //val = string(r)
-        //if val != "0" {
-            //if len(val+modifier) < 3 { // is a normal number
-                //letters += words[val+modifier]
-            //} else if len(val+modifier) < 4 {
-                //letters += words[val]
-            //} else {
-                //letters += words[val]
-            //}
-        //} 
-        //modifier += "0"
-    //}
-    //if len(runes) >= 3 {
-        //if string(runes[0]) != "0" || string(runes[1]) != "0" {
-            //letters += 3
-        //}
-        //if len(runes) >= 4 {
-            //letters += words["thousand"]
-        //} else {
-            //letters += words["hundred"]
-        //}
-    //}
-    //return letters
