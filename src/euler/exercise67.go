179dad2fc50297b961a88c71206a0307c2e8337d
Sam Rogerson <samr@cern.ch>
2013-04-24 17:04:31 +0100

    web modules

--- src/euler/exercise67.go
+++ src/euler/exercise67.go
@@ -2,16 +2,12 @@
 package euler
 
 import (
-    "net/http"
-    "io/ioutil"
     "euler/triangles"
+    "euler/web"
 )
 
 func Exercise67() int {
-    triweb, _ := http.Get("http://projecteuler.net/project/triangle.txt")
-    defer triweb.Body.Close()
-    tritext, _ := ioutil.ReadAll(triweb.Body)
-    tri := string(tritext)
+    tri := web.FetchText("http://projecteuler.net/project/triangle.txt")
     lines := triangles.Splittext(tri)
     nlines := len(lines)
     max, _ := triangles.Maxroute(lines[1:nlines])
